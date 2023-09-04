fa5506915e353cddde1f97c0ed573c9d63d88688
Hadi-SayedAhmad <121687873+Hadi-SayedAhmad@users.noreply.github.com>
2023-09-04 16:59:13 +0300

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Heading from "./heading.jsx";
+import List from "./unOrderedList.jsx";
+function App() {
+  return (
+    <div>
+      <Heading />
+      <List />
+    </div>
+  );
+}
+export default App;
---
+++ src/components/unOrderedList.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+const list = (
+  <ul>
+    <li>Bacon</li>
+    <li>Jamon</li>
+    <li>Noodles</li>
+  </ul>
+);
+function UnOrderedList() {
+  return list;
+}
+export default UnOrderedList;
